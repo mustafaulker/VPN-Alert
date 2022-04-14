7617c267c82a653226a0dd2b5b26af3291a9665a
mustafaulker <mustafaulker.cs@gmail.com>
2022-04-15 00:44:44 +0100

    Add layout to the UI

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import urllib.request
 
 from PyQt6.QtCore import QUrl
 from PyQt6.QtMultimedia import QSoundEffect
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QLabel, QWidget, QTextEdit
 
 external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
@@ -13,20 +13,40 @@ class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
 
+        self.activate_button = QPushButton("Activate")
+        self.textArea = QTextEdit()
         self.effect = QSoundEffect()
+
+        self.textArea.setReadOnly(True)
+
         self.effect.setSource(QUrl.fromLocalFile("beep.wav"))
         self.effect.setLoopCount(-2)
 
-        self.start_button = QPushButton("Start")
-        self.start_button.clicked.connect(self.start)
+        self.init_ui()
 
-        self.start_button.setCheckable(True)
+    def init_ui(self):
+        self.setWindowTitle("VPN Alert")
+        self.resize(450, 300)
+        self.ui_components()
+        self.show()
 
-        self.setCentralWidget(self.start_button)
+    def ui_components(self):
+        layout = QVBoxLayout()
+
+        self.activate_button.clicked.connect(self.start)
+        self.activate_button.setCheckable(True)
+
+        layout.addWidget(self.textArea)
+        layout.addWidget(self.activate_button)
+
+        w = QWidget()
+        w.setLayout(layout)
+
+        self.setCentralWidget(w)
 
     def start(self):
-        if self.start_button.isChecked():
-            self.start_button.setText("Stop")
+        if self.activate_button.isChecked():
+            self.activate_button.setText("Deactivate")
 
             global external_ip
             current_external_ip = copy.copy(external_ip)
@@ -41,13 +61,10 @@ class Main(QMainWindow):
             self.effect.play()
         else:
             self.effect.stop()
-            self.start_button.setText("Start")
+            self.activate_button.setText("Activate")
             print("Alert has deactivated at: " + time.strftime("%H:%M:%S"))
 
 
 app = QApplication([])
-
 window = Main()
-window.show()
-
 app.exec()
