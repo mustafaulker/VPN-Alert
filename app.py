61e5591ae8c3a5816ec4fd7969eea8d0677285e2
mustafaulker <mustafaulker.cs@gmail.com>
2022-04-15 00:22:01 +0100

    Fix typo

--- app.py
+++ app.py
@@ -13,37 +13,35 @@ class Main(QMainWindow):
     def __init__(self):
         super(Main, self).__init__()
 
-        global effect
-        effect = QSoundEffect()
-        effect.setSource(QUrl.fromLocalFile("beep.wav"))
-        effect.setLoopCount(-2)
+        self.effect = QSoundEffect()
+        self.effect.setSource(QUrl.fromLocalFile("beep.wav"))
+        self.effect.setLoopCount(-2)
 
-        global start_button
-        start_button = QPushButton("Start")
-        start_button.clicked.connect(self.start)
+        self.start_button = QPushButton("Start")
+        self.start_button.clicked.connect(self.start)
 
-        start_button.setCheckable(True)
+        self.start_button.setCheckable(True)
 
-        self.setCentralWidget(start_button)
+        self.setCentralWidget(self.start_button)
 
     def start(self):
-        if start_button.isChecked():
-            start_button.setText("Stop")
+        if self.start_button.isChecked():
+            self.start_button.setText("Stop")
 
             global external_ip
             current_external_ip = copy.copy(external_ip)
-            
+
             print("Alert has activated at: " + time.strftime("%H:%M:%S"))
 
             while current_external_ip == external_ip:
                 current_external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
                 time.sleep(2)
 
-            print("Discconected from VPN at: " + time.strftime("%H:%M:%S"))
-            effect.play()
+            print("Disconnected from the VPN at: " + time.strftime("%H:%M:%S"))
+            self.effect.play()
         else:
-            effect.stop()
-            start_button.setText("Start")
+            self.effect.stop()
+            self.start_button.setText("Start")
             print("Alert has deactivated at: " + time.strftime("%H:%M:%S"))
 
 
